c092a4895e3ac5851b970fd89a4f1ce45ebf0f7c
Menna Selim <mennakhalilselim@gmail.com>
2025-10-25 00:26:00 +0300

    fresh spring boot app

---
+++ src/main/java/com/evision/filesimilarity/FileSimilarityApplication.java
@@ -0,0 +1,13 @@
+package com.evision.filesimilarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSimilarityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSimilarityApplication.class, args);
+    }
+
+}
